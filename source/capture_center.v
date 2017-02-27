aada4da5b54e20dba0389fc85be208202f1ef251
Kevin Balke <kbalke@ucla.edu>
2017-02-27 13:46:30 -0800

    Initial commit

---
+++ source/capture_center.v
@@ -0,0 +1,35 @@
+module capture_center (
+    input clk,
+    input rst,  // reset
+    input signal,
+    
+    input [WIDTH - 1:0] counter,
+    output reg [WIDTH - 1:0] center_out,
+    output reg ready
+  );
+  
+  parameter WIDTH = 32;
+  
+  reg [WIDTH - 1:0] center_buffer;
+  
+  reg signal_d;
+  
+  always @(posedge clk) begin
+    if (rst) begin
+      center_out <= 0;
+      ready <= 0;
+      signal_d <= 0;
+    end else begin
+      if (!signal && signal_d) begin // Falling edge
+        center_out <= (center_buffer + counter) / 2;
+        ready <= 1;
+      end
+      
+      if (signal && !signal_d) begin // Rising edge
+        center_buffer <= counter;
+        ready <= 0;
+      end
+      signal_d <= signal;
+    end
+  end
+endmodule
---
+++ source/spi_memory.v
@@ -0,0 +1,39 @@
+module spi_memory (
+    input clk,  // clock
+    input rst,  // reset
+    input reset_addr,
+    input incr,
+    input [15 : 0] F, 
+    input [15 : 0] C, 
+    input [15 : 0] L, 
+    input [15 : 0] R,
+    output [7:0] out_byte
+  );
+  
+  wire [7 : 0] bytes [7:0];
+  
+  reg [3:0] addr;
+  
+  reg incr_d;
+  
+  assign bytes[0] = {F[15:8],F[7:0]};
+  assign bytes[1] = {C[15:8],C[7:0]};
+  assign bytes[2] = {L[15:8],L[7:0]};
+  assign bytes[3] = {R[15:8],R[7:0]};
+  
+  always @(posedge clk) begin
+    if (rst || reset_addr) begin
+      addr <= 0;
+      incr_d <= 0;
+    end else begin
+      if (incr && !incr_d) begin
+        addr <= addr + 1;
+      end
+    end
+    
+    incr_d <= incr;
+  end
+  
+  assign out_byte = bytes[addr];
+  
+endmodule
